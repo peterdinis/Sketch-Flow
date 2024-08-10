72bfdbd31d0e5dfd390a3b067098004778383b8c
Peter Dinis <peterdinis0@pobox.sk>
2024-08-10 17:27:21 +0200

    infra: Setup files

--- app/_components/shared/Room.tsx
+++ app/_components/shared/Room.tsx
@@ -1,17 +1,25 @@
-"use client";
+'use client';
 
-import { ReactNode } from "react";
-import { ClientSideSuspense } from "@liveblocks/react";
-import { LiveMap } from "@liveblocks/client";
-import { Loader2 } from "lucide-react";
-import { RoomProvider } from "@/liveblocks.config";
+import { ReactNode } from 'react';
+import { ClientSideSuspense } from '@liveblocks/react';
+import { LiveMap } from '@liveblocks/client';
+import { Loader2 } from 'lucide-react';
+import { RoomProvider } from '@/liveblocks.config';
 
 export function Room({ children }: { children: ReactNode }) {
     return (
-        <RoomProvider id="my-room" initialPresence={{ cursor: null, cursorColor: null, editingText: null }}
-            initialStorage={{ canvasObjects: new LiveMap(), }}
+        <RoomProvider
+            id='my-room'
+            initialPresence={{
+                cursor: null,
+                cursorColor: null,
+                editingText: null,
+            }}
+            initialStorage={{ canvasObjects: new LiveMap() }}
         >
-            <ClientSideSuspense fallback={<Loader2 className="animate-spin w-8 h-8" />}>
+            <ClientSideSuspense
+                fallback={<Loader2 className='h-8 w-8 animate-spin' />}
+            >
                 {() => children}
             </ClientSideSuspense>
         </RoomProvider>
